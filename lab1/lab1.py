30cfa2ab60383bc7fa1322318149eca7745691ee
KolesnikovAlexsandr <mister.sasha.kolesnikov@yandex.ru>
2016-11-06 02:24:24 +0300

    add network and DataSet

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -1,4 +1,6 @@
-import pybrain
+from pybrain.tools.shortcuts import buildNetwork
+from pybrain.supervised.trainers import BackpropTrainer
+from pybrain.datasets import SupervisedDataSet
 import numpy
 
 def x_3(x):
@@ -6,15 +8,18 @@ def x_3(x):
 
 
 def main():
-    i = -1
+    i = 0
     x = [ ]
     y = [ ]
-
-    while i < 1:
+    y_out = []
+    ds = SupervisedDataSet(1, 1)
+    while i < 0.5:
         x.append( i )
         y.append(x_3( i ))
         i = i + 0.1
-        
+        ds.addSample(x[x.len],y[y.len])
+    network = buildNetwork(1, 1, 1)
+
     print x
     print y
 
