3cc6a4cfbd00196302a3292fad6422120e7a3496
KolesnikovAlexsandr <mister.sasha.kolesnikov@yandex.ru>
2016-11-15 02:09:29 +0300

    all work but i not sure

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -2,7 +2,7 @@ from pybrain.tools.shortcuts import buildNetwork
 from pybrain.supervised.trainers import BackpropTrainer
 from pybrain.datasets import SupervisedDataSet
 from pybrain.structure import FullConnection
-from pybrain.structure import SigmoidLayer
+from pybrain.structure import LinearLayer
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -15,30 +15,31 @@ def main():
     i = -1
     x = [ ]
     y = [ ]
-    y_out = []
+    x_out = []
     ds = SupervisedDataSet(1, 1)
     while i < 1:
         x.append( i )
         y.append(x_3( i ))
-        i = i + 0.01
+        i = i + 0.1
         ds.addSample(i,x_3(i))
 
-    network = buildNetwork(1 ,1, 1, bias=True , recurrent=True , hiddenclass=SigmoidLayer)
+
+    network = buildNetwork(1 ,1, 1, bias=True , recurrent=True ,hiddenclass=LinearLayer )
     recCon = FullConnection(network['in'], network['hidden0'])
     network.addConnection(recCon)
     network.sortModules()
 
-    print network['in']
-    print network['hidden0']
-    print network['out']
     trainer = BackpropTrainer(network, ds,learningrate=0.05)
 
 
     trainer.trainEpochs(1000)
 
-    print network.activate([0])
+    i = -1
+    while i < 1:
+        x_out.append( network.activate([i]) )
+        i = i+0.1
 
-    plt.plot(x,y)
+    plt.plot(x, y, 'b', x_out, y, 'r')
     plt.show()
 
 
