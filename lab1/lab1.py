3a784496adbd9148480c139dce8fd65c49919616
KolesnikovAlexsandr <mister.sasha.kolesnikov@yandex.ru>
2016-11-15 01:49:49 +0300

    add plot and small change of networck but it don't work

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -1,27 +1,45 @@
 from pybrain.tools.shortcuts import buildNetwork
 from pybrain.supervised.trainers import BackpropTrainer
 from pybrain.datasets import SupervisedDataSet
-import numpy
+from pybrain.structure import FullConnection
+from pybrain.structure import SigmoidLayer
+import numpy as np
+import matplotlib.pyplot as plt
+
 
 def x_3(x):
     return x*x*x
 
 
 def main():
-    i = 0
+    i = -1
     x = [ ]
     y = [ ]
     y_out = []
     ds = SupervisedDataSet(1, 1)
-    while i < 0.5:
+    while i < 1:
         x.append( i )
         y.append(x_3( i ))
-        i = i + 0.1
+        i = i + 0.01
         ds.addSample(i,x_3(i))
-    network = buildNetwork(1, 1, 1)
 
-    print x
-    print y
+    network = buildNetwork(1 ,1, 1, bias=True , recurrent=True , hiddenclass=SigmoidLayer)
+    recCon = FullConnection(network['in'], network['hidden0'])
+    network.addConnection(recCon)
+    network.sortModules()
+
+    print network['in']
+    print network['hidden0']
+    print network['out']
+    trainer = BackpropTrainer(network, ds,learningrate=0.05)
+
+
+    trainer.trainEpochs(1000)
+
+    print network.activate([0])
+
+    plt.plot(x,y)
+    plt.show()
 
 
 
