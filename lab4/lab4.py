1759d5ff590cfe3dd835f20ef9be877ccb4dfaab
KolesnikovAlexsandr <mister.sasha.kolesnikov@yandex.ru>
2016-11-23 02:36:05 +0300

    add function for create ds and start develop network

--- lab4/lab4.py
+++ lab4/lab4.py
@@ -2,27 +2,59 @@ from pybrain.tools.shortcuts import buildNetwork
 from pybrain.supervised.trainers import BackpropTrainer
 from pybrain.datasets import SupervisedDataSet
 from pybrain.structure import FullConnection
-from pybrain.structure import LinearLayer
+from pybrain.structure import SoftmaxLayer
+from pybrain.structure import TanhLayer
 import numpy as np
 import matplotlib.pyplot as plt
+
+TableZamena = []
+
+ds = SupervisedDataSet(48, 26)
+def make_data():
+
+    trash = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+    file = open('ABC_template.txt', 'r')
+    number_of_line = 0
+    counter = 0
+    for line in file:
+
+        if number_of_line%3 == 0 :
+
+            lineElement = line.split(" ")
+
+            trash[counter] = 1
+
+            TableZamena.append(lineElement.pop(0))
+
+            b = 0
+            while b < len(lineElement):
+                lineElement[b] = int(lineElement[b])
+                b = b + 1
+
+            ds.addSample(lineElement, trash)
+            trash[counter] = 0
+
+            counter = counter + 1
+        number_of_line = number_of_line + 1
+
+    file.close()
 
 
 
 def main():
-    f = open('ABC_template.txt', 'r')
-    ds = SupervisedDataSet(1, 1)
-    ds.addSample()
+    make_data()
+
+    network = buildNetwork(48 ,1, 26 , hiddenclass=TanhLayer )
+
+    trainer = BackpropTrainer(network, ds)
 
 
-    network = buildNetwork(1 ,1, 1, bias=True , recurrent=True ,hiddenclass=LinearLayer )
-    recCon = FullConnection(network['in'], network['hidden0'])
-    network.addConnection(recCon)
-    network.sortModules()
+    trainer.trainEpochs(500)
 
-    trainer = BackpropTrainer(network, ds,learningrate=0.05)
-
-
-    trainer.trainEpochs(1000)
+    counter = 0
+    t = network.activate([1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1])
+    print t
 
 
 main()
