4faa34ed8c6a4ed38a8ae42df67ad1f73147bb27
KolesnikovAlexsandr <mister.sasha.kolesnikov@yandex.ru>
2016-11-23 17:55:43 +0300

    add func for worck with main file but networck return bad data

--- lab4/lab4.py
+++ lab4/lab4.py
@@ -10,6 +10,8 @@ import matplotlib.pyplot as plt
 TableZamena = []
 
 ds = SupervisedDataSet(48, 26)
+
+network = buildNetwork(48 , 26 )
 def make_data():
 
     trash = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
@@ -25,7 +27,7 @@ def make_data():
 
             trash[counter] = 1
 
-            TableZamena.append(lineElement.pop(0))
+            TableZamena.append(lineElement.pop(0)[0])
 
             b = 0
             while b < len(lineElement):
@@ -54,19 +56,50 @@ def indexMax(mass):
 
 
 
+def trainNetworck():
+    trainer = BackpropTrainer(network, ds)
+    trainer.trainEpochs(1000)
+
+
+def work():
+    trash = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0 , 0 , 0 , 0 , 0 , 0]
+    file = open('Neural_network_homework.txt', 'r')
+    counter = 0
+    string  = ""
+    for l in file:
+        i = 0
+        string = string + l
+    string = string+"   "
+    i = 0
+    result = ""
+    while i < len(string):
+        if string[i] == "{":
+            counter = 0
+        elif string[i] == "1" or string[i] == "0":
+            trash[counter] = int(string[i])
+            counter = counter + 1
+        elif string[i] == "}" or counter == 48:
+            result = result + TableZamena[indexMax(network.activate(trash))]
+            counter = 0
+        elif string[i] == '"' and string[i+1] == " " and string[i+2] == '"':
+            result = result + " "
+            i=i+2
+
+        i = i + 1
+
+    print result
+
 def main():
     make_data()
-
-    network = buildNetwork(48 , 26 )
-
-    trainer = BackpropTrainer(network, ds)
-
-
-    trainer.trainEpochs(500)
-
-    counter = 0
-    t = network.activate([1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1,1,0,1,1,0,1,1,0,1,1,0,1,1,0,1,1,0,1,0,1,0,0,1,0,0,1,0,0,1,0])
-    print TableZamena[indexMax(t)]
+    trainNetworck()
+    trash = [1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1]
+    print TableZamena[indexMax(network.activate(trash))]
+    trash = [1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,1,1,0,1,1,1,0,1]
+    print TableZamena[indexMax(network.activate(trash))]
+    trash = [1,1,1,1,0,0,1,0,0,0,1,0,1,0,0,0,0,1,1,0,0,0,1,1,1,1,1,1,0,0,1,0,1,0,0,0,1,0,0,1,0,0,1,0,0,0,1,0]
+    print TableZamena[indexMax(network.activate(trash))]
+    work()
 
 
 main()
