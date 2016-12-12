6ad8926e6d316f82bd1ccf725cfc736f293c2f64
KolesnikovAlexsandr <mister.sasha.kolesnikov@yandex.ru>
2016-12-13 01:09:05 +0300

    end 1 lab(not sure)

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -1,8 +1,11 @@
 from pybrain.tools.shortcuts import buildNetwork
 from pybrain.supervised.trainers import BackpropTrainer
+from pybrain.supervised.trainers import Trainer
 from pybrain.datasets import SupervisedDataSet
+from pybrain.structure import GaussianLayer
 from pybrain.structure import FullConnection
 from pybrain.structure import LinearLayer
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -15,7 +18,7 @@ def main():
     i = -1
     x = [ ]
     y = [ ]
-    x_out = []
+    y_out = []
     ds = SupervisedDataSet(1, 1)
     while i < 1:
         x.append( i )
@@ -24,22 +27,26 @@ def main():
         ds.addSample(i,x_3(i))
 
 
-    network = buildNetwork(1 ,1, 1, bias=True , recurrent=True ,hiddenclass=LinearLayer )
-    recCon = FullConnection(network['in'], network['hidden0'])
-    network.addConnection(recCon)
+    network = buildNetwork(1 ,1, 1, bias=True , recurrent=True  ,outclass=LinearLayer )
+    recCon = FullConnection(network['out'], network['hidden0'])
+    network.addRecurrentConnection(recCon)
     network.sortModules()
 
     trainer = BackpropTrainer(network, ds,learningrate=0.05)
+    #trainer = Trainer(network , ds , learningrate=0.06)
 
 
-    trainer.trainEpochs(1000)
+    trainer.trainUntilConvergence()
 
     i = -1
     while i < 1:
-        x_out.append( network.activate([i]) )
-        i = i+0.1
+        a = network.activate([i])
+        print a
+        print i
+        y_out.append(a)
+        i = i + 0.1
 
-    plt.plot(x, y, 'b', x_out, y, 'r')
+    plt.plot(x, y, 'b', y_out , y ,  'r')
     plt.show()
 
 
