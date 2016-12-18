0a0c7728edcd685f0dfed8ba2c5cc24a6bab9cb6
KolesnikovAlexsandr <mister.sasha.kolesnikov@yandex.ru>
2016-12-18 19:10:00 +0300

    end develop 4 lab and end all project

--- lab4/lab4.py
+++ lab4/lab4.py
@@ -5,7 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pylab as pl
 
-def make_data(Zamena , DataIn ):
+def make_data( Zamena , DataIn ):
 
     trash = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
@@ -26,7 +26,6 @@ def make_data(Zamena , DataIn ):
             while b < len(lineElement):
                 lineElement[b] = int(lineElement[b])
                 b = b + 1
-
             DataIn.append(lineElement)
             i = 0
             j = 0
@@ -36,7 +35,18 @@ def make_data(Zamena , DataIn ):
         number_of_line = number_of_line + 1
     file.close()
 
-def indexMax(mass):
+def indexMax(DataIn , DataOut):
+    mass = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    k = 0
+    while k < 26:
+        counter = 0
+        j = 0
+        while j < 46:
+            j = j+1
+            if DataIn[k][j] == DataOut[j]:
+                counter = counter +1
+        mass[k] = counter
+        k = k+1
     i = 0
     index = 0
     max = mass[0]
@@ -50,67 +60,61 @@ def indexMax(mass):
 
 
 
-def work(Data):
+def work( DataOut , TableZamena):
     trash = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
              0, 0, 0, 0, 0, 0, 0 , 0 , 0 , 0 , 0 , 0]
+
     file = open('Neural_network_homework.txt', 'r')
     counter = 0
     string  = ""
     for l in file:
-        i = 0
         string = string + l
+
     string = string+"   "
     i = 0
     result = ""
-    while i < len(string):
-        if string[i] == "{":
+    while i < len( string ):
+        if string[ i ] == "{":
             counter = 0
-        elif string[i] == "1" or string[i] == "0":
-            trash[counter] = int(string[i])
+        elif string[ i ] == "1" or string[ i ] == "0":
+            trash[ counter ] = int( string[ i ] )
             counter = counter + 1
-        elif string[i] == "}" or counter == 48:
-            Data.append(trash)
+        elif string[ i ] == "}" or counter == 48:
+            result = result + TableZamena[indexMax(DataOut,trash)]
             counter = 0
-        elif string[i] == '"' and string[i+1] == " " and string[i+2] == '"':
-            i=i+2
-
+        elif string[ i ] == '"' and string[ i + 1 ] == " " and string[ i + 2 ] == '"':
+            result = result + " "
+            i = i + 2
+        elif string[ i ] == '"' and string[ i + 1 ] == "," and (string[ i + 2 ] == ' ' or string[ i+2 ] == '"'):
+            result = result + ","
+            i = i + 3
         i = i + 1
-
+    print "Message:"
     print result
+
+
+
+
 
 def main():
     TableZamena = []
     DataTrain = []
-    DataOut = []
-    make_data(TableZamena,DataTrain)
+    make_data(TableZamena , DataTrain)
+    work( DataTrain , TableZamena)
 
-    DataTrain = np.asfarray(DataTrain)
-    DataTrain[DataTrain == 0] = -1
-    net = nl.net.newhop(DataTrain)
-    DataOut = net.sim(DataTrain)
-    DataWork = []
-    #work( net , TableZamena , DataTrain , DataOut )
-    j = 0
-    while j < 26:
-        out = net.sim([DataTrain[j]])
-        print ((out[0] == DataOut[0]).all(), TableZamena[j], len(net.layers[0].outs))
-        j=j+1
-    work(DataWork)
-    k = 0
-    test = []
-    result = ""
-    print DataWork
-    a = DataWork[0]
-    trash = net.sim(a)
-    test = np.asfarray(trash)
-    test[test == 0] = -1
-    while k < 26:
-        if (test[0] == DataOut[j]).all():
-            result = result + TableZamena[j]
-            print result
-        k=k+1
+
+
+
+
+
+
+
+
+
 
 
 
 
 main()
+
+
